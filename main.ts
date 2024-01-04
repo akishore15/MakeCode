73bfdc0bddda73962f0f7bd3c7a3209c03ff642d
Aravind Kishore <154870205+akishore15@users.noreply.github.com>
2024-01-03 20:56:09 -0800

    Update pxt.json, main.blocks, main.ts, images.g.jres, images.g.ts

--- main.ts
+++ main.ts
@@ -2,9 +2,14 @@ namespace SpriteKind {
     export const Gem = SpriteKind.create()
 }
 controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
+    info.setLife(3)
+    info.startCountdown(60)
     scene.setBackgroundImage(assets.image`mine`)
+    music.play(music.createSong(assets.song`music1`), music.PlaybackMode.LoopingInBackground)
+    controller.moveSprite(Diamond)
 })
-let Diamond = sprites.create(img`
+let Diamond: Sprite = null
+Diamond = sprites.create(img`
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
